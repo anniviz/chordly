e55b58177438aa3ad0b80c0132dc1b60dbb432cb
Annemarie Wisbar <a.wisbar@gmail.com>
2019-11-25 11:24:22 +0100

    Add styled components and GlobalStyles

---
+++ src/GlobelStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+}
+
+body {
+  background: #26304A;
+  color: FEFEFE;
+  font-family: sans-serif;
+}
+`
