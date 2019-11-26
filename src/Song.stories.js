23b66c7b211eb9091200a1707c80f02033a4b822
Annemarie Wisbar <a.wisbar@gmail.com>
2019-11-26 16:21:26 +0100

    Implemented storybook

--- src/Song.stories.js
+++ src/Song.stories.js
@@ -10,8 +10,11 @@ export default {
 
 export const standard = () => (
   <Song
-    title={text('Songtitle', 'This is the title of a song')}
-    artist={text('Song artist', 'This is the artist of a song')}
-    lyrics={text('Song lyrics', 'This are the lyrics of a song')}
+    title={text('Songtitle', 'All Day')}
+    artist={text('Song artist', 'Hillsong United')}
+    lyrics={text(
+      'Song lyrics',
+      "{start_of_verse: Verse 1}   \n[G#m] I don't [E]care what they [B]say about [F#]me   \nIt's alright, alright   \nI don't care what they think about me   \nIt's alright, they'll get it one day   \n{end_of_verse: Verse 1}   \n   \n{start_of_chorus: Pre-Chorus}   \nI love you, I'll follow You   \nYou are my, my life   \nI will read my bible and pray   \nI will follow you all day   \n{end_of_chorus: Pre-Chorus}   \n   \n{start_of_verse: Verse 2}   \nI don't care what it costs anymore   \nCos' You gave it all and I'm following You   \nI don't care what it takes anymore   \nNo matter what happens I'm going your way   \n{end_of_verse: Verse 2}   \n   \n{start_of_chorus: Pre-Chorus}   \nPre-Chorus   \n{end_of_chorus: Pre-Chorus}   \n   \n{start_of_chorus: Chorus}   \n[B] All [E]day,[B] all [F#]day now,[B] all [E]day[G#m][E]   \n{end_of_chorus: Chorus}   \n   \n{start_of_chorus: Bridge}   \n[C#m] Any[E]one around can [C#m]see just how [E]good You've been to me   \n[C#m] For all my [E]friends that don't know [C#m]You, I pray that [E]You would save them, [B]too   \n{end_of_chorus: Bridge}"
+    )}
   />
 )
---
+++ src/TitleList.stories.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+import { withInfo } from '@storybook/addon-info'
+import { action } from '@storybook/addon-actions'
+import TitleList from './TitleList'
+
+export default {
+  title: 'TitleList',
+  decorators: [withKnobs, withInfo],
+}
+
+export const standard = () => (
+  <TitleList
+    songs={[
+      {
+        title: 'All Day',
+        artist: 'Hillsong United',
+        lyrics:
+          "{start_of_verse: Verse 1}   \n[G#m] I don't [E]care what they [B]say about [F#]me   \nIt's alright, alright   \nI don't care what they think about me   \nIt's alright, they'll get it one day   \n{end_of_verse: Verse 1}   \n   \n{start_of_chorus: Pre-Chorus}   \nI love you, I'll follow You   \nYou are my, my life   \nI will read my bible and pray   \nI will follow you all day   \n{end_of_chorus: Pre-Chorus}   \n   \n{start_of_verse: Verse 2}   \nI don't care what it costs anymore   \nCos' You gave it all and I'm following You   \nI don't care what it takes anymore   \nNo matter what happens I'm going your way   \n{end_of_verse: Verse 2}   \n   \n{start_of_chorus: Pre-Chorus}   \nPre-Chorus   \n{end_of_chorus: Pre-Chorus}   \n   \n{start_of_chorus: Chorus}   \n[B] All [E]day,[B] all [F#]day now,[B] all [E]day[G#m][E]   \n{end_of_chorus: Chorus}   \n   \n{start_of_chorus: Bridge}   \n[C#m] Any[E]one around can [C#m]see just how [E]good You've been to me   \n[C#m] For all my [E]friends that don't know [C#m]You, I pray that [E]You would save them, [B]too   \n{end_of_chorus: Bridge}",
+      },
+    ]}
+    displayedSong={text('Song artist', 'This is the artist of a song')}
+    setDisplayedSong={text('Song lyrics', 'This are the lyrics of a song')}
+  />
+)
