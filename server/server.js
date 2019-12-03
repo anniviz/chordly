4a5d1224deeaef9d98a9fafd9a5b5fb929af3cac
Annemarie Wisbar <a.wisbar@gmail.com>
2019-12-03 16:35:04 +0100

    Clearup dependencies again

--- server/server.js
+++ server/server.js
@@ -4,12 +4,23 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/songnet', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 
 const app = express()
 app.use(express.json())
+
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Express ready on ${PORT}`))
+
 app.get('/songs', (req, res) => {
-  Song.find().then(songs => res.json(songs))
+  Song.find()
+    .then(songs => res.json(songs))
+    .catch(err => res.json(err))
+})
+
+app.get('/songs/:id', (req, res) => {
+  Song.findById(req.params._id)
+    .then(song => res.json(song))
+    .catch(err => res.json(err))
 })
