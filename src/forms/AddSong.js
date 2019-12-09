8949d8fa488bf43ca5ddcaa51e6cd1eec6160001
Annemarie Wisbar <a.wisbar@gmail.com>
2019-12-09 15:08:46 +0100

    Update serialize-javascript

--- src/forms/AddSong.js
+++ src/forms/AddSong.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-import Layout from '../common/Layout'
-
 export default function AddSong() {
   return (
     <AddForm method="post">
