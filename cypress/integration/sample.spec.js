7e6e99ce2b524c78ff455a1afe8ba064621076d0
Annemarie Wisbar <a.wisbar@gmail.com>
2020-01-16 14:23:08 +0100

    Add test to open the SideList

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,11 +1,16 @@
-describe("My First Test", function() {
-  it("Visits my App", function() {
-    cy.visit("http://localhost:3000/");
+describe('My First Test', function() {
+  it('Visits my App', function() {
+    cy.visit('http://localhost:3000/')
 
-    cy.contains("Song");
-  });
+    cy.contains('Song')
+  })
 
-  it("Looks for my title", function() {
-    cy.title().should("include", "chordly");
-  });
-});
+  it('Looks for my title', function() {
+    cy.title().should('include', 'chordly')
+  })
+
+  it('has a button to open the sidelist', () => {
+    cy.get('#list-button').click()
+    // cy.contains('Come Away').click()
+  })
+})
