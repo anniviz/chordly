9933ac35675e0ad8c465d2e1658047371b732723
Annemarie Wisbar <a.wisbar@gmail.com>
2020-01-16 10:25:12 +0100

    Changes after CodeReview

--- src/common/Layout.js
+++ src/common/Layout.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components/macro'
 
 export default styled.div`
-  /* display: flex;
-  align-items: center; */
-  /* justify-content: center; */
   display: grid;
   grid-template-columns: auto auto;
   align-items: center;
