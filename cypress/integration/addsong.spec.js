ab4226ecc7f34ac80c9f0d58e786950a49dc0553
Annemarie Wisbar <a.wisbar@gmail.com>
2020-01-18 11:12:26 +0100

    Extend addSong test

--- cypress/integration/addsong.spec.js
+++ cypress/integration/addsong.spec.js
@@ -3,11 +3,13 @@ describe('Test add song function', function() {
     cy.visit('http://localhost:3000/')
     cy.contains('Loading ...').should('not.exist')
     cy.get('#list-button').click()
-  })
 
-  it('open the search field', () => {
     cy.get('.add-icon')
       .should('be.visible')
       .click()
+  })
+
+  it('text area is focused', () => {
+    cy.get('textarea').should('have.focus')
   })
 })
